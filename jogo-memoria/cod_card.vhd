a1ef02208930eed27b4ec2e33ec5bc1980d49210
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-12-04 19:38:47 -0300

    Update cod_card.vhd

--- jogo-memoria/cod_card.vhd
+++ jogo-memoria/cod_card.vhd
@@ -3,15 +3,16 @@ PORT(	enable			: IN BIT;						-- enable
 		position   		: IN NATURAL;        			-- natural input
 		escolheu		: OUT BIT;						-- duas cartas escolhidas
 		pos_a, pos_b   	: OUT NATURAL;  				-- data output
-		num_card   		: BUFFER BIT 					-- buffer
+		num_card   		: BUFFER BIT; 					-- buffer
+		last_pos 		: BUFFER NATURAL				-- ultima posição		
 );
 END cod_card;
 
 ARCHITECTURE behavior OF cod_card IS
 BEGIN
-	PROCESS(enable, position)
+	PROCESS(position, enable)
 		BEGIN
-		IF (enable = '1') THEN
+		IF ((enable = '1') AND NOT (position = last_pos)) THEN
 			IF(num_card = '0') THEN
 				num_card <= '1';
 				pos_a <= position;
@@ -21,8 +22,11 @@ BEGIN
 				pos_b <= position;
 				escolheu <= '1';
 			END IF;
+			last_pos <= position;
 		ELSE
 			num_card <= num_card;
+			last_pos <= last_pos;
+			escolheu <= '0';
 		END IF;
 	END PROCESS;
 END behavior;
