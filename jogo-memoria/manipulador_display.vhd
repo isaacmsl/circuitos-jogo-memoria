430825be9a2661138547dc9ad2585502fb9ccd86
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2023-12-04 09:44:14 -0300

    manipulador display usa cartas_jogo

--- jogo-memoria/manipulador_display.vhd
+++ jogo-memoria/manipulador_display.vhd
@@ -20,20 +20,20 @@ ARCHITECTURE arch OF manipulador_display IS
     SIGNAL s8, s9, s10, s11, s12, s13, s14, s15 : BIT_VECTOR(6 DOWNTO 0);
     
     BEGIN
-        d0 : dec_display PORT MAP (cartas_viradas(0), "000", s0);
-        d1 : dec_display PORT MAP (cartas_viradas(1), "000", s1);
-        d2 : dec_display PORT MAP (cartas_viradas(2), "000", s2);
-        d3 : dec_display PORT MAP (cartas_viradas(3), "000", s3);
-        d4 : dec_display PORT MAP (cartas_viradas(4), "000", s4);
-        d5 : dec_display PORT MAP (cartas_viradas(5), "000", s5);
-        d6 : dec_display PORT MAP (cartas_viradas(6), "000", s6);
-        d7 : dec_display PORT MAP (cartas_viradas(7), "000", s7);
-        d8 : dec_display PORT MAP (cartas_viradas(8), "000", s8);
-        d9 : dec_display PORT MAP (cartas_viradas(9), "000", s9);
-        d10 : dec_display PORT MAP (cartas_viradas(10), "000", s10);
-        d11 : dec_display PORT MAP (cartas_viradas(11), "000", s11);
-        d12 : dec_display PORT MAP (cartas_viradas(12), "000", s12);
-        d13 : dec_display PORT MAP (cartas_viradas(13), "000", s13);
-        d14 : dec_display PORT MAP (cartas_viradas(14), "000", s14);
-        d15 : dec_display PORT MAP (cartas_viradas(15), "000", s15);
+        d0 : dec_display PORT MAP (cartas_viradas(0), cartas_jogo(0), s0);
+        d1 : dec_display PORT MAP (cartas_viradas(1), cartas_jogo(1), s1);
+        d2 : dec_display PORT MAP (cartas_viradas(2), cartas_jogo(2), s2);
+        d3 : dec_display PORT MAP (cartas_viradas(3), cartas_jogo(3), s3);
+        d4 : dec_display PORT MAP (cartas_viradas(4), cartas_jogo(4), s4);
+        d5 : dec_display PORT MAP (cartas_viradas(5), cartas_jogo(5), s5);
+        d6 : dec_display PORT MAP (cartas_viradas(6), cartas_jogo(6), s6);
+        d7 : dec_display PORT MAP (cartas_viradas(7), cartas_jogo(7), s7);
+        d8 : dec_display PORT MAP (cartas_viradas(8), cartas_jogo(8), s8);
+        d9 : dec_display PORT MAP (cartas_viradas(9), cartas_jogo(9), s9);
+        d10 : dec_display PORT MAP (cartas_viradas(10), cartas_jogo(10), s10);
+        d11 : dec_display PORT MAP (cartas_viradas(11), cartas_jogo(11), s11);
+        d12 : dec_display PORT MAP (cartas_viradas(12), cartas_jogo(12), s12);
+        d13 : dec_display PORT MAP (cartas_viradas(13), cartas_jogo(13), s13);
+        d14 : dec_display PORT MAP (cartas_viradas(14), cartas_jogo(14), s14);
+        d15 : dec_display PORT MAP (cartas_viradas(15), cartas_jogo(15), s15);
 END arch;
